3fad8d61ed3b9678c5c30182eff738cb90f3a190
Kővágó, Zoltán <DirtY.iCE.hu@gmail.com>
2011-01-15 18:07:57 +0100

    Little hack to work on gentoo, where llvm libs are not in a standard path.
    It won't work on systems where shared objects are not 'libfoo.so'

--- lib/llvm.rb
+++ lib/llvm.rb
@@ -6,12 +6,12 @@ module LLVM
 
     # load required libraries
     begin
-      ffi_lib 'LLVM-2.8'
+      ffi_lib `llvm-config --libdir`.strip + '/libLLVM-2.8.so'
     rescue LoadError
-      ffi_lib 'LLVM-2.7'
+      ffi_lib `llvm-config --libdir`.strip + '/libLLVM-2.7.so'
     end
   end
-  
+
   NATIVE_INT_SIZE = case FFI::Platform::ARCH
     when "x86_64" then 64
     # PPC, other arches?
